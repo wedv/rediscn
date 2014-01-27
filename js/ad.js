e16fa1453ec2f4e988c75864fb531957e2480bad
geelou <rjgcs123@gmail.com>
2014-01-27 16:37:50 +0800

    添加新年祝福
    添加新年祝福

--- js/ad.js
+++ js/ad.js
@@ -5,8 +5,8 @@ function hiddenAddiv(){
 }
 
 var adTHML = "";
-adTHML += "     <div id='showaddiv' style='display:;height:100px;padding-top:100px;' class='text columns home sidebar'>";
-adTHML += "          <h1>热烈庆祝 redis.cn 于 <font color='#cc0000'>2011 年 11 月 11 日 11 时 11 分</font> 正式上线<h1>";
+adTHML += "     <div id='showaddiv' style='display:;' class='text columns home sidebar'>";
+adTHML += "          <img src='/images/newYear2014.jpg' />";
 adTHML += "      </div>";
 document.write(adTHML);
 
