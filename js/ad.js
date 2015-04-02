bee4b0f296a6bdc94067ce92fa0f709b2725cadc
geelou <rjgcs123@gmail.com>
2015-04-02 09:48:25 +0800

    更新版本到3.0
    更新版本到3.0

--- js/ad.js
+++ js/ad.js
@@ -6,8 +6,8 @@ function hiddenAddiv(){
 
 var adTHML = "";
 adTHML += "     <div id='showaddiv' style='display:;margin: 0 auto;max-width: 900px;' class='columns home sidebar'>";
-adTHML += "          <img src='/images/christmas.jpg' />";
+adTHML += "          <img src='/images/year2015.jpg' width='900' height='660'/>";
 adTHML += "      </div>";
 document.write(adTHML);
 
-setTimeout("hiddenAddiv()",5000);
+setTimeout("hiddenAddiv()",10000);
